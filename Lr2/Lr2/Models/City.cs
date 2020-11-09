145c60e75ae1314ebbaf7bdcad1b730c0bb07089
margoboiko <56150348+margoboiko@users.noreply.github.com>
2020-11-10 00:40:10 +0200

    Add files via upload

---
+++ Lr2/Lr2/Models/City.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lr2.Models
+{
+    public partial class City
+    {
+        public City()
+        {
+            Calling = new HashSet<Calling>();
+        }
+
+        public int Id { get; set; }
+        public string City1 { get; set; }
+        public int? Code { get; set; }
+        public decimal? Tariff { get; set; }
+
+        public virtual ICollection<Calling> Calling { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder city = new StringBuilder()
+                .Append(Id)
+                .Append("   ")
+                .Append(City1)
+                .Append("   ")
+                .Append(Code)
+                .Append("   ")
+                .Append(Tariff);
+            return city.ToString();
+        }
+    }
+}
