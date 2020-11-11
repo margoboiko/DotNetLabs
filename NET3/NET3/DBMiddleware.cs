278c76e0dab8ef58ee00b2a72d0a040b911ca4cd
margoboiko <56150348+margoboiko@users.noreply.github.com>
2020-11-11 10:03:07 +0200

    Add files via upload

---
+++ NET3/NET3/DBMiddleware.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NET3.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace NET3
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class DBMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public DBMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, phone_callContext db)
+        {
+            try
+
+            {
+                httpContext.Response.ContentType = "text/html; charset=utf-8";
+
+                await httpContext.Response.WriteAsync("<img src='/images/logo.png' width=100 heigh=100 alt='My image' />");
+                await httpContext.Response.WriteAsync("<h2>ТЕЛЕФОННІ ДЗВІНКИ</h2>");
+
+                var person = db.Person.ToList();
+
+                await httpContext.Response.WriteAsync("</br><h4>Список користувачів:</h4>");
+
+                await httpContext.Response.WriteAsync(String.Join("</br>", person));
+
+
+                await httpContext.Response.WriteAsync("</br></br><h4>Список 5 міст:</h4>");
+                var city = db.City.ToList();
+                await httpContext.Response.WriteAsync(string.Join("</br>", city));
+
+
+                await httpContext.Response.WriteAsync("</br></br><h4>Список користувачів, які здійснили хоча б 2 дзвінки в різні міста:</h4>");
+                var calling = db.Person
+                        .Join(db.Calling, person => person.Id, calling => calling.Personid,
+                        (p, c) => new { PersonId = p.Id, Surname = p.Surname, Name = p.Name, FName = p.Fname, c.Personid })
+                        .ToList()
+                        .GroupBy(table => new { table.PersonId, table.Surname, table.Name, table.FName })
+                        .Where(g => g.Count() >= 2);
+
+                foreach (var p in calling)
+                {
+                    await httpContext.Response.WriteAsync($"{p.Key.PersonId}   {p.Key.Surname}    {p.Key.Name}    {p.Key.FName} " + $"Дзвінки: {p.Count()}</br>");
+                }
+            }
+            catch (Exception ex)
+            {
+                await httpContext.Response.WriteAsync($"Error: {ex.Message}");
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<DBMiddleware>();
+        }
+    }
+}
---
+++ NET3/NET3/Models/Calling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET3.Models
+{
+    public partial class Calling
+    {
+        public int Id { get; set; }
+        public DateTime? Data { get; set; }
+        public string During { get; set; }
+        public int? Personid { get; set; }
+        public int? Cityid { get; set; }
+
+        public virtual City City { get; set; }
+        public virtual Person Person { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder calling = new StringBuilder()
+                .Append(Id)
+                .Append("   ")
+                .Append(Data)
+                .Append("   ")
+                .Append(During)
+                .Append("   ")
+                .Append(Personid)
+                .Append("   ")
+                .Append(Cityid);
+            return calling.ToString();
+        }
+    }
+}
